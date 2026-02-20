c82174dda99389516c77b93cc38c01df6720b6f3
Adripeinado3 <adrianpeinadoc.sanjosemlg@fundacionloyola.net>
2026-02-20 13:31:46 +0100

    Clase contacto agregada

--- RepositorioEjer1/src/agenda/Contacto.java
+++ RepositorioEjer1/src/agenda/Contacto.java
@@ -2,5 +2,39 @@ package agenda;
 
 public class Contacto {
 
-  
+    private String nombre;
+    private String telefono;
+    private String email;
+
+    public void Contacto(String nombre, String telefono, String email) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.email = email;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    // Setters
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
 }
