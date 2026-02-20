34b5638284af1b3083d909d52698302abe1c68e8
Adripeinado3 <adrianpeinadoc.sanjosemlg@fundacionloyola.net>
2026-02-20 13:30:46 +0100

    Metodo agregar contacto agregado

--- RepositorioEjer1/src/agenda/Agenda.java
+++ RepositorioEjer1/src/agenda/Agenda.java
@@ -4,4 +4,13 @@ import java.util.ArrayList;
 
 public class Agenda {
 	
+    ArrayList<Contacto> contactos = new ArrayList<>();
+	
+    public void agregarContacto(Contacto c) {
+       
+        if (c.getNombre() != null && c.getEmail() != null && c.getTelefono() != null) {
+            
+            contactos.add(c);
+        }
+    }
 }
