479c64a6497ba617a39b9da36b698b7504652703
Tu Nombre <tuemail@ejemplo.com>
2026-02-20 13:32:19 +0100

    Metodos buscar y eliminar en la clase Agenda

--- RepositorioEjer1/src/agenda/Agenda.java
+++ RepositorioEjer1/src/agenda/Agenda.java
@@ -4,4 +4,22 @@ import java.util.ArrayList;
 
 public class Agenda {
 	
+	public void eliminarContacto(String nombre) {
+    	for ( int i = 0; i < contactos.size();i++) {
+    		if (contactos.get(i).getNombre().contentEquals(nombre)) {
+    				contactos.remove(i);
+    		}
+    	}
+    }
+    
+    public Contacto buscarContacto(String nombre) {
+    	for (Contacto c : contactos) {
+    		if (c.getNombre().equals(nombre)) {
+    			return c;
+    		}
+    	}
+    	return null;
+    	
+    	
+    }
 }
